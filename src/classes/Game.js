e62e6620382581b9436c9fa2e16cebcf0fe6987a
dzinoviev <danzino21@gmail.com>
2021-01-26 21:45:36 +0200

    Update: finish the game when snake eat itself

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -1,15 +1,15 @@
-import { Apple, Snake } from '/';
+import { Apple, Snake } from "/";
 
 function Game(opts) {
   const defaultOptions = {
     cellSize: 15,
     totalWidth: 500,
     totalHeight: 300,
-    snakeColor: '#235621',
-    appleColor: '#aa0000',
+    snakeColor: "#235621",
+    appleColor: "#aa0000",
     gameSpeed: 100,
     ctx: null,
-    canMoveThroughWalls: true
+    canMoveThroughWalls: true,
   };
   const options = Object.assign(defaultOptions, opts);
   const {
@@ -20,42 +20,32 @@ function Game(opts) {
     appleColor,
     gameSpeed,
     ctx,
-    canMoveThroughWalls
+    canMoveThroughWalls,
   } = options;
   this.snake = new Snake();
   this.apples = [];
   this.initialized = false;
   this.totalRows = Math.floor(totalWidth / cellSize);
   this.totalCols = Math.floor(totalHeight / cellSize);
-  
 
-  this.checkEndGame = function() {
-    const { x, y } = this.snake.getLastTile();
-    if ( ! canMoveThroughWalls ) {
-      if (
-        x < 0 || x >= this.totalRows ||
-        y < 0 || y >= this.totalCols
-      ) {
-        this.end();
-      }
+  this.init = function () {
+    if (this.initialized) {
+      return console.log("The game was initialized yet!");
     }
-  }
+    this.start();
 
-  this.getAppleRandomPosition = function() {
-    const x = Math.floor(Math.random() * this.totalRows);
-    const y = Math.floor(Math.random() * this.totalCols);
-    if (this.snake.tiles.some(tile => x === tile.x && y === tile.y)) {
-      return this.getAppleRandomPosition();
-    }
-    return [x, y];
-  }
-
+    window.addEventListener("keydown", (e) => {
+      this.updateSnakeDirection(e.code);
+    });
+    console.log("Game init has been done.");
+    this.initialized = true;
+  };
   this.update = function () {
     // Create apples
     if (this.apples.length === 0) {
       const [x, y] = this.getAppleRandomPosition();
 
-      this.apples.push(new Apple({x, y}))
+      this.apples.push(new Apple({ x, y }));
     }
 
     // Update snake position
@@ -76,15 +66,15 @@ function Game(opts) {
           break;
       }
     }
-    this.totalRows, this.totalCols
+    this.totalRows, this.totalCols;
     this.snake.move(next, this.apples);
 
     // Check that snake can eat apples
     const tile = this.snake.getLastTile();
-    this.apples = this.apples.filter(({x, y}) => {
+    this.apples = this.apples.filter(({ x, y }) => {
       return x !== tile.x || y !== tile.y;
     });
-    
+
     // Check end game
     this.checkEndGame();
   };
@@ -92,10 +82,10 @@ function Game(opts) {
   this.render = function () {
     // console.log("run render");
     ctx.clearRect(0, 0, totalWidth, totalHeight);
-    this.apples.forEach(({x, y}) => {
+    this.apples.forEach(({ x, y }) => {
       ctx.fillStyle = appleColor;
       ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-    })
+    });
     this.snake.tiles.forEach((tile) => {
       ctx.fillStyle = snakeColor;
       ctx.fillRect(tile.x * cellSize, tile.y * cellSize, cellSize, cellSize);
@@ -104,25 +94,69 @@ function Game(opts) {
 
   this.tick = function () {
     try {
-      this.update(this.snake);
-      this.render(this.snake);
+      this.update();
+      this.render();
     } catch (e) {
       console.error(e.message);
     }
   };
 
-  this.updateSnakeDirection = function(key) {
+  this.start = function () {
+    console.log("Game init was started.");
+    this.interval = setInterval(() => this.tick(), gameSpeed);
+    this.tick();
+    document.body.classList.add("playing");
+  };
+
+  this.checkEndGame = function () {
+    const { x, y } = this.snake.getLastTile();
+
+    // If cannot move through walls
+    if (!canMoveThroughWalls) {
+      if (x < 0 || x >= this.totalRows || y < 0 || y >= this.totalCols) {
+        this.end();
+      }
+    }
+    // If snake eat itself
+    var positions = {};
+    this.snake.tiles.forEach(({ x, y }) => {
+      const uniquePosition = `${x}, ${y}`;
+      if (positions[uniquePosition]) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+        this.end();
+      }
+      positions[uniquePosition] = true;
+    });
+  };
+
+  this.end = function () {
+    console.log("The game has ended.");
+    clearInterval(this.interval);
+    document.body.classList.remove("playing");
+  };
+
+  this.getAppleRandomPosition = function () {
+    const x = Math.floor(Math.random() * this.totalRows);
+    const y = Math.floor(Math.random() * this.totalCols);
+    if (this.snake.tiles.some((tile) => x === tile.x && y === tile.y)) {
+      return this.getAppleRandomPosition();
+    }
+    return [x, y];
+  };
+
+  this.updateSnakeDirection = function (key) {
     // [x, y]
     const keyMap = {
-      ArrowDown:  [0, 1],
-      KeyS:       [0, 1],
-      ArrowUp:    [0, -1],
-      KeyW:       [0, -1],
+      ArrowDown: [0, 1],
+      KeyS: [0, 1],
+      ArrowUp: [0, -1],
+      KeyW: [0, -1],
       ArrowRight: [1, 0],
-      KeyD:       [1, 0],
-      ArrowLeft:  [-1, 0],
-      KeyA:       [-1, 0],
-    }
+      KeyD: [1, 0],
+      ArrowLeft: [-1, 0],
+      KeyA: [-1, 0],
+    };
     if (keyMap[key]) {
       const [x, y] = keyMap[key];
       const head = this.snake.getLastTile();
@@ -137,32 +171,6 @@ function Game(opts) {
       this.snake.moveX = x;
       this.snake.moveY = y;
     }
-  }
-
-  this.end = function() {
-    console.log('The game has ended.');
-    clearInterval(this.interval);
-    document.body.classList.remove('playing');
-  }
-
-  this.start = function() {
-    console.log("Game init was started.");
-    this.interval = setInterval(() => this.tick(), gameSpeed);
-    this.tick();
-    document.body.classList.add('playing');
-  }
-
-  this.init = function () {
-    if (this.initialized) {
-      return console.log("The game was initialized yet!");
-    }
-    this.start();
-
-    window.addEventListener('keydown', (e) => {
-      this.updateSnakeDirection(e.code)
-    });
-    console.log("Game init has been done.");
-    this.initialized = true;
   };
 }
 
